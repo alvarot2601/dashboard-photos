2fcb7726c13c6aaeede7cc498a9a16c6f3ca2b7e
alvarot2601 <alvarot2601@gmail.com>
2022-11-16 15:08:31 +0100

    doy mejor formato al código de ciertos archivos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import { React, createContext, useState } from "react";
+import { React } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./componentes/Home.js";
 import Search from "./componentes/Search.js";
 import Root from "./componentes/Root.js";
 import MyPhotos from "./componentes/MyPhotos.js";
 
-
-
-const imagesContext = createContext();
-
 const App = () => {
-  const [images, setImages] = useState([]);
   return (
-    <imagesContext.Provider value={images} >
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path="/" element={<Root />}>
-            <Route index element={<Home />} />
-            <Route path="search" element={<Search imagesContext={imagesContext} images = {images} setImages={setImages} />} />
-            <Route path="myphotos" element={<MyPhotos />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </imagesContext.Provider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Home />} />
+          <Route path="search" element={<Search />} />
+          <Route path="myphotos" element={<MyPhotos />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 export default App;
